5db0457eb76f2273fb969d6643bf64727f8e0458
KaiWenyy <43819040+KaiWenyy@users.noreply.github.com>
2019-12-17 00:45:53 +0800

    update read_and_save_data() & load_data()

--- preproc.py
+++ preproc.py
@@ -3,52 +3,60 @@ import os
 import sys
 import pandas as pd 
 import json
-import numpy as np 
+import numpy as np
+import pickle 
 
-def read_data():
-    #df = pd.read_csv('train_v2_split.csv')
-    df = pd.read_csv('train_5000r.csv')  
-    #print(df.head())  
-    simple_col = ['channelGrouping', 'date', 'fullVisitorId', 'socialEngagementType',
-    'visitId', 'visitNumber', 'visitStartTime']
-    full_col = ['customDimensions', 'device', 'geoNetwork', 'hits', 'trafficSource', 'totals'] + simple_col
+def read_and_save_data(csv_paths, df_pickle_name):
+
+    all_df = pd.DataFrame()
     allVisitorId = {} # (key: id, value: login times)
-    allchannelGroup = df.channelGrouping.unique()
-    index_range = df.index
+    for path in csv_paths:
+        df = pd.read_csv(path) 
+        #print(df.head())  
+        simple_col = ['channelGrouping', 'date', 'fullVisitorId', 'socialEngagementType',
+        'visitId', 'visitNumber', 'visitStartTime']
+        full_col = ['customDimensions', 'device', 'geoNetwork', 'hits', 'trafficSource', 'totals'] + simple_col
+        index_range = df.index
 
-    for col in full_col:
-        if col in simple_col:
-           #print('simple: ',col,  df[col].dtype)
-            if col == 'fullVisitorId':
-                for index in df.index:
-                    if df.at[index, col] in allVisitorId.keys():
-                        allVisitorId[ df.at[index, col] ] += 1
-                    else:
-                        allVisitorId[ df.at[index, col] ] = 1
-        else: 
-            #pass
-            #print('notsimple: ',col, df[col].dtype)
-            for index in index_range:
-                
-                if col == 'customDimensions' or col == 'hits':
-                    df.at[index, col] = df.at[index, col].replace(" '", ' "').replace("['", '["').replace("{'", '{"').replace("',", '",').replace("':",'":').replace("'}",'"}').replace("']",'"]').replace("True", 'true')
-                    #.replace('Men"s',"Men's").replace('Women"s',"Women's").replace('Kid"s',"Kid's").replace('Kids"',"Kids'")
-                #print(index, col)
-                val_dict = json.loads(df.at[index, col])
-                df.at[index, col] = val_dict
+        for col in full_col:
+            if col in simple_col:
+               #print('simple: ',col,  df[col].dtype)
+                if col == 'fullVisitorId':
+                    for index in df.index:
+                        if df.at[index, col] in allVisitorId.keys():
+                            allVisitorId[ df.at[index, col] ] += 1
+                        else:
+                            allVisitorId[ df.at[index, col] ] = 1
+            else: 
+                #print('notsimple: ',col, df[col].dtype)
+                for index in index_range:
+                    
+                    if col == 'customDimensions' or col == 'hits':
+                        df.at[index, col] = df.at[index, col].replace('"', "'").replace(" '", ' "').replace("['", '["').replace("{'", '{"').replace("',", '",').replace("':",'":').replace("'}",'"}').replace("']",'"]').replace("True", 'true')
+                    
+                    print("process", path, index, col)
+                    val_dict = json.loads(df.at[index, col])
+                    df.at[index, col] = val_dict
 
-                #drop the subject without label
-                if col == 'totals':
-                    if 'transactionRevenue' not in df.at[index, col]:
-                        #pass
-                        df.drop([index], inplace = True)
+                    #drop the subject without label
+                    if col == 'totals':
+                        if 'transactionRevenue' not in df.at[index, col]:
+                            df.drop([index], inplace = True)
+                df.reset_index()
 
-            df.reset_index()
-    print(len(allVisitorId.keys()))
-    print(allchannelGroup)
-    #print(allVisitorId)
-    return df, allVisitorId
+            if col == full_col[-1]:
+                all_df = pd.concat([all_df,df],axis=0, ignore_index=True)
 
+    all_df.to_pickle(df_pickle_name)
+    print("save DataFrame to", df_pickle_name)
+    allVisitorId.to_pickle('allVisitorId.pkl')
+    print("save allVisitorId to allVisitorId.pkl")
+    print("number of visitor ID:", len(allVisitorId.keys()))
+
+def load_data(pickle_path):
+    print('loading', pickle_path + ' ...') 
+    df = pd.read_pickle(pickle_path)
+    return df
 
 def get_one_feature(df, column="totals", subcolumn=None, to_float=False):
     if subcolumn != None:
@@ -80,10 +88,11 @@ def get_target(df, allVisitorId):
 
 
 def main():
-    df, allVisitorId = read_data()
+    #csv_paths = ['train_split/train_split' + str(j+1) + '.csv' for j in range(101)]
+    #read_and_save_data(csv_paths, 'df.pkl')
+    df = load_data('df.pkl')
+    allVisitorId = load_data('allVisitorId.pkl')
+    print(len(df.index))
+    print(len(allVisitorId.keys()))
 
-
-
-
-
-
+main()
