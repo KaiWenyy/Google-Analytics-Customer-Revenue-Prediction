b844cdd639a9ddf5ff98b7602bfdc5e2406a9703
KaiWenyy <43819040+KaiWenyy@users.noreply.github.com>
2019-12-17 00:58:24 +0800

    split and save multiple files

--- csvsplit.py
+++ csvsplit.py
@@ -3,25 +3,27 @@ import os
 import sys
 
 csv.field_size_limit(sys.maxsize)
-path = '/Users/Ivy/Downloads/train_v2.csv'
-
+path = '/Users/Ivy/Desktop/datascience/final/train_v2.csv'
+j = 51
+loadStart = 250000
+totalFiles = 100
 with open(path, 'r', newline='') as file:
     csvreader = csv.reader(file)
     a = next(csvreader)
     #print(a)
-    i = j = 1
+    i = 1
     for row in csvreader:
-        print(i)
-        if i <= 5000:
-            #j += 1
-            csv_path = os.path.join('/'.join(path.split('/')[:-1]), 'train_v2_split' + '.csv')
-            # print('/'.join(path.split('/')[:-1]))
+        if i <= loadStart:
+            i += 1
+        elif i <= 5000*j:
+            print(i)
+            csv_path = os.path.join('/'.join(path.split('/')[:-1]), 'train_split/train_split' + str(j) + '.csv')
             #print(csv_path)
             # 不存在此文件的時候，就創建
             if not os.path.exists(csv_path):
                 with open(csv_path, 'w', newline='') as file:
                     csvwriter = csv.writer(file)
-                    #csvwriter.writerow(['image_url'])
+                    csvwriter.writerow(['channelGrouping','customDimensions','date','device','fullVisitorId','geoNetwork','hits','socialEngagementType','totals','trafficSource','visitId','visitNumber','visitStartTime'])
                     csvwriter.writerow(row)
                 i += 1
             # 存在的時候就往裏面添加
@@ -30,5 +32,7 @@ with open(path, 'r', newline='') as file:
                     csvwriter = csv.writer(file)
                     csvwriter.writerow(row)
                 i += 1
-        else:
+        if i == 5000*j:
+            j += 1
+        elif j >= totalFiles:
             break
